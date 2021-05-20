93be22e01e74b3152e761731eb1afd4ded18a5ad
zhoufan123 <zhoufan123@babytree-inc.com>
2021-05-20 09:49:35 +0800

    initialize

---
+++ Pods/Target Support Files/Pods-WechatDemo-WechatDemoUITests/Pods-WechatDemo-WechatDemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WechatDemo_WechatDemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_WechatDemo_WechatDemoUITests
+@end
---
+++ WechatDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WechatDemo
+//
+//  Created by FranZhou on 2021/5/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
